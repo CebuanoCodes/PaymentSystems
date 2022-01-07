7636f39353c10732d47508908709603b92b13d53
CebuanoCodes <96157842+CebuanoCodes@users.noreply.github.com>
2022-01-07 02:12:44 +0000

    Update ServiceExtensions.cs

--- PaymentSystem/PaymentSystem.Api/Extensions/ServiceExtensions.cs
+++ PaymentSystem/PaymentSystem.Api/Extensions/ServiceExtensions.cs
@@ -7,16 +7,16 @@ namespace PaymentSystem.Api.Extensions
 {
     public static class ServiceExtensions
     {
-        //public static void ConfigureCors(this IServiceCollection services)
-        //{
-        //    services.AddCors(options =>
-        //    {
-        //        options.AddPolicy("CorsPolicy",
-        //            builder => builder.AllowAnyOrigin()
-        //            .AllowAnyMethod()
-        //            .AllowAnyHeader());
-        //    });
-        //}
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
 
 
         public static void ConfigureLoggerService(this IServiceCollection services)
