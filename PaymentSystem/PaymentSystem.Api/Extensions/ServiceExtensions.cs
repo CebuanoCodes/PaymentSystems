e19682b6f9ffd2c11205edc0cd5a633b76890b15
CebuanoCodes <96157842+CebuanoCodes@users.noreply.github.com>
2022-01-07 01:47:49 +0000

    Add files via upload

---
+++ PaymentSystem/PaymentSystem.Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Core;
+using LoggerService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PaymentSystem.Api.Extensions
+{
+    public static class ServiceExtensions
+    {
+        //public static void ConfigureCors(this IServiceCollection services)
+        //{
+        //    services.AddCors(options =>
+        //    {
+        //        options.AddPolicy("CorsPolicy",
+        //            builder => builder.AllowAnyOrigin()
+        //            .AllowAnyMethod()
+        //            .AllowAnyHeader());
+        //    });
+        //}
+
+
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+
+
+    }
+}
