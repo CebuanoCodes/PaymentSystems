0b6ed7ae2912e392b75e83c4e325ad8424123db7
CebuanoCodes <96157842+CebuanoCodes@users.noreply.github.com>
2022-01-07 01:57:08 +0000

    Update UserController.cs

--- PaymentSystem/PaymentSystem.Api/Controllers/UserController.cs
+++ PaymentSystem/PaymentSystem.Api/Controllers/UserController.cs
@@ -1,4 +1,7 @@
 using System.Threading.Tasks;
+using AutoMapper;
+using Core.DTO;
+using LoggerService;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PaymentSystem.Core.Models;
@@ -11,22 +14,32 @@ namespace PaymentSystem
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
+        private IMapper _mapper;
+        private ILoggerManager _logger;
 
-        public UserController(IUserService userService)
+
+        public UserController(IUserService userService, IMapper mapper, ILoggerManager logger)
         {
             this._userService = userService;
-        }
+            this._mapper = mapper;
+            this._logger = logger;
 
+        }
 
         [HttpGet("{id}")]
         //[Authorize]
         public async Task<ActionResult<User>> GetUserById(int id)
         {
+            //this will be save in the log folder of the registered path in the nlog.config file
+             _logger.LogInfo($"Returned user with id: {id}");
 
+            //declaring a variable
             var user = await _userService.GetUserById(id);
 
+            //this will return the user with the parameter inputted with the Status response.
             return Ok(user);
+                        
         }
-
+      
     }
 }
